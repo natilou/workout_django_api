b10671dd9977b429b245dcd0b7447f1bb5bb53f6
natilou <30585029+natilou@users.noreply.github.com>
2023-05-15 22:31:04 -0300

    Creating django project and app
    - Django app: api
    - Django project: workout
    - Adding models, urls, views, filters and serializers
    - Adding initial migration
    - Adding project settings

---
+++ workout/api/serializers.py
@@ -0,0 +1,81 @@
+from rest_framework import serializers
+
+from .models import (
+    Category,
+    Equipment,
+    Exercise,
+    Force,
+    Image,
+    Level,
+    Mechanic,
+    Muscle,
+)
+
+
+class MuscleGroupSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Muscle
+        fields = ["id", "name"]
+
+
+class CategorySerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Category
+        fields = ["id", "name"]
+
+
+class MechanicSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Mechanic
+        fields = ["id", "name"]
+
+
+class EquipmentSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Equipment
+        fields = ["id", "name"]
+
+
+class LevelSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Level
+        fields = ["id", "name"]
+
+
+class ForceSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Force
+        fields = ["id", "name"]
+
+
+class ExerciseSerializer(serializers.HyperlinkedModelSerializer):
+    category = CategorySerializer()
+    mechanic = MechanicSerializer()
+    equipment = EquipmentSerializer()
+    level = LevelSerializer()
+    force = ForceSerializer()
+    primary_muscles = MuscleGroupSerializer(many=True)
+    secondary_muscles = MuscleGroupSerializer(many=True)
+
+    class Meta:
+        model = Exercise
+        fields = [
+            "id",
+            "name",
+            "instructions",
+            "category",
+            "mechanic",
+            "equipment",
+            "level",
+            "force",
+            "primary_muscles",
+            "secondary_muscles",
+        ]
+
+
+class ImageSerializer(serializers.HyperlinkedModelSerializer):
+    exercise = ExerciseSerializer()
+
+    class Meta:
+        model = Image
+        fields = ["id", "name", "exercise"]
---
+++ workout/api/views.py
@@ -0,0 +1,79 @@
+from django_filters import rest_framework as filters
+from rest_framework import generics, viewsets
+
+from .filters import ExerciseFilter
+from .models import Category, Equipment, Exercise, Force, Level, Mechanic, Muscle
+from .serializers import (
+    CategorySerializer,
+    EquipmentSerializer,
+    ExerciseSerializer,
+    ForceSerializer,
+    LevelSerializer,
+    MechanicSerializer,
+    MuscleGroupSerializer,
+)
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows Category to be viewed or edited.
+    """
+
+    queryset = Category.objects.all().order_by("name")
+    serializer_class = CategorySerializer
+
+
+class EquipmentViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows Equipment to be viewed or edited.
+    """
+
+    queryset = Equipment.objects.all().order_by("name")
+    serializer_class = EquipmentSerializer
+
+
+class LevelViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows Level to be viewed or edited.
+    """
+
+    queryset = Level.objects.all().order_by("name")
+    serializer_class = LevelSerializer
+
+
+class MechanicViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows Mechanic to be viewed or edited.
+    """
+
+    queryset = Mechanic.objects.all().order_by("name")
+    serializer_class = MechanicSerializer
+
+
+class MuscleViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows Muscle to be viewed or edited.
+    """
+
+    queryset = Muscle.objects.all().order_by("name")
+    serializer_class = MuscleGroupSerializer
+
+
+class ForceViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows Force to be viewed or edited.
+    """
+
+    queryset = Force.objects.all().order_by("name")
+    serializer_class = ForceSerializer
+
+
+class ExerciseViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows Exercise to be viewed or edited.
+    """
+
+    queryset = Exercise.objects.all().order_by("name")
+    serializer_class = ExerciseSerializer
+    filter_backends = [filters.DjangoFilterBackend]
+    filterset_class = ExerciseFilter
