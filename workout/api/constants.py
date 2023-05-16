cc1ceeb31830ed962b289ff51d9a4ffa093cfdd8
natilou <30585029+natilou@users.noreply.github.com>
2023-05-15 22:34:39 -0300

    Fixtures
    - Adding fixtures to populate data base with json files
    - Adding a custom command
    - Adding migrations which populate the data base

---
+++ workout/api/constants.py
@@ -0,0 +1,9 @@
+models = {
+    "category": "api.category",
+    "level": "api.level",
+    "mechanic": "api.mechanic",
+    "equipment": "api.equipment",
+    "primaryMuscles": "api.muscle",
+    "images": "api.image",
+    "force": "api.force",
+}
