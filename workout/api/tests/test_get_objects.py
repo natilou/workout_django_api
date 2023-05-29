4817372e10dc061ad8d3e2f408897381e339f5d9
natilou <30585029+natilou@users.noreply.github.com>
2023-05-29 15:29:17 -0300

    Added unit test
    - Using pytest and pytest-django
    - Adding `conftest.py` and `pytest.ini` files
    - For now, adding only basic tests to get data. Later I will add permissions and verify them

---
+++ workout/api/tests/test_get_objects.py
@@ -0,0 +1,44 @@
+from api.models import Category, Equipment, Force, Level, Mechanic, Muscle
+from rest_framework.test import APIClient
+
+
+def test_get_level_list(api_client_user, Level):
+    resp = api_client_user.get("/levels/")
+
+    assert resp.status_code == 200
+    assert len(resp.json()["results"]) == 3
+
+
+def test_get_equipment_list(api_client_user, Equipment):
+    resp = api_client_user.get("/equipments/")
+
+    assert resp.status_code == 200
+    assert len(resp.json()["results"]) == 5
+
+
+def test_get_category_list(api_client_user, Category):
+    resp = api_client_user.get("/categories/")
+
+    assert resp.status_code == 200
+    assert len(resp.json()["results"]) == 7
+
+
+def test_get_mechanic_list(api_client_user, Mechanic):
+    resp = api_client_user.get("/mechanics/")
+
+    assert resp.status_code == 200
+    assert len(resp.json()["results"]) == 2
+
+
+def test_get_muscles_list(api_client_user, Muscle):
+    resp = api_client_user.get("/muscles/")
+
+    assert resp.status_code == 200
+    assert len(resp.json()["results"]) == 10
+
+
+def test_get_forces_list(api_client_user, Force):
+    resp = api_client_user.get("/forces/")
+
+    assert resp.status_code == 200
+    assert len(resp.json()["results"]) == 3
---
+++ workout/conftest.py
@@ -0,0 +1,128 @@
+import os
+
+import django
+import pytest
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "workout.settings")
+django.setup()
+from rest_framework.test import APIClient
+
+
+@pytest.fixture()
+def User(django_user_model):
+    return django_user_model
+
+
+@pytest.fixture()
+def admin_user(User):
+    return User.objects.create_user(
+        email="admintest@admin.com",
+        username="admintest",
+        is_staff=True,
+        is_superuser=True,
+        password="password",
+    )
+
+
+@pytest.fixture()
+def client_user(User):
+    return User.objects.create_user(
+        email="user@mail.com",
+        username="testuser",
+        password="password",
+    )
+
+
+@pytest.fixture()
+def api_client_admin(admin_user):
+    client = APIClient()
+    client.force_authenticate(user=admin_user)
+    return client
+
+
+@pytest.fixture()
+def api_client_user(client_user):
+    client = APIClient()
+    client.force_authenticate(user=client_user)
+    return client
+
+
+@pytest.fixture()
+def Level():
+    from api.models import Level
+
+    Level.objects.create(name="Beginner")
+    Level.objects.create(name="Intermediate")
+    Level.objects.create(name="Expert")
+
+    return Level
+
+
+@pytest.fixture()
+def Equipment():
+    from api.models import Equipment
+
+    Equipment.objects.create(name="bands")
+    Equipment.objects.create(name="barbell")
+    Equipment.objects.create(name="cable")
+    Equipment.objects.create(name="kettlebells")
+    Equipment.objects.create(name="machine")
+
+    return Equipment
+
+
+@pytest.fixture()
+def Category():
+    from api.models import Category
+
+    Category.objects.create(name="cardio")
+    Category.objects.create(name="plyometrics")
+    Category.objects.create(name="powerlifting")
+    Category.objects.create(name="strength")
+    Category.objects.create(name="stretching")
+    Category.objects.create(name="strongman")
+    Category.objects.create(name="olympic weightlifting")
+
+    return Category
+
+
+@pytest.fixture()
+def Mechanic():
+    from api.models import Mechanic
+
+    Mechanic.objects.create(name="compound")
+    Mechanic.objects.create(name="isolation")
+
+    return Mechanic
+
+
+@pytest.fixture()
+def Muscle():
+    from api.models import Muscle
+
+    Muscle.objects.create(name="abductors")
+    Muscle.objects.create(name="abs")
+    Muscle.objects.create(name="biceps")
+    Muscle.objects.create(name="chest")
+    Muscle.objects.create(name="forearms")
+    Muscle.objects.create(name="glutes")
+    Muscle.objects.create(name="lats")
+    Muscle.objects.create(name="triceps")
+    Muscle.objects.create(name="shoulders")
+    Muscle.objects.create(name="neck")
+
+    return Muscle
+
+
+@pytest.fixture()
+def Force():
+    from api.models import Force
+
+    Force.objects.create(name="pull")
+    Force.objects.create(name="push")
+    Force.objects.create(name="static")
+
+    return Force
+
+
+
